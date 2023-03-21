b0e4ae65021f47312ce8aa73a6eddb0828065053
Nemzytch <lesieutre.kevin@gmail.com>
2023-03-21 15:16:13 +0100

    RANDOM

--- src/functions/disable-scroll.ts
+++ src/functions/disable-scroll.ts
@@ -1,16 +1,16 @@
 export const disableMainScroll = function (elems = []) {
-  document.querySelector('body').classList.add('no-scroll');
+  document?.querySelector('body')?.classList.add('no-scroll');
 }
 
 export const enableMainScroll = function () {
   let i = 0;
-  document.querySelector('#contact-us.opened') && ++i;
-  document.querySelector('.privacy-policy.opened') && ++i;
-  document.querySelector('#purchase-mini.step-1') && ++i;
-  document.querySelector('#purchase-mini.step-2-3') && ++i;
-  document.querySelector('#header-mini.opened') && ++i;
+  document?.querySelector('#contact-us.opened') && ++i;
+  document?.querySelector('.privacy-policy.opened') && ++i;
+  document?.querySelector('#purchase-mini.step-1') && ++i;
+  document?.querySelector('#purchase-mini.step-2-3') && ++i;
+  document?.querySelector('#header-mini.opened') && ++i;
   if(i > 1) {
     return;
   }
-  document.querySelector('body').classList.remove('no-scroll');
+  document?.querySelector('body')?.classList.remove('no-scroll');
 }
