b138eda8e4a228112c207d5ba9970c17c2d4870b
kratoar <mael.fallet@gmail.com>
2023-03-30 18:01:41 +0200

    Date automatique dans le pied de page

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -89,7 +89,7 @@ const Footer = ({}:Footer) => {
                     })}
                 </div>
                 <div className="footer-bottom-navigation">
-                    <div className="footer-trademark">© {new Date().getUTCFullYear()} INMODEMD</div>
+                    <div className="footer-trademark">© {new Date().getUTCFullYear()} INMODE</div>
                     <div className="footer-navigation">
                         {footer.navigation && footer.navigation.map((menu, key) => {
                             return (
