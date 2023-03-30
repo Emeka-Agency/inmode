7444b650aafa3a0e59bc58b158cd87e5e58daf60
kratoar <mael.fallet@gmail.com>
2023-03-30 17:57:04 +0200

    Lien de contact dans le header à droite

--- src/components/contexts/menus-provider.tsx
+++ src/components/contexts/menus-provider.tsx
@@ -389,7 +389,7 @@ const MenusProvider = ({ requested = "", children }:{ requested?:string, childre
     const [menusHeaderTop] = React.useState(process_menu(datas.header_top.nodes));
     const [menusHeaderBottom] = React.useState(
       process_menu(datas.header_bottom.nodes.map(elem => elem))
-      .sort((a, b) => a.url == "/contact" ? 1 : -1)
+      .sort((a, b) => b.url == "/contact" ? -1 : 0)
     );
     const [menusFooter] = React.useState(datas.footer);
 
