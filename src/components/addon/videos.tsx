f784f17ca615e6f4763a894f7e034407017c01f1
kratoar <mael.fallet@gmail.com>
2023-03-30 17:59:08 +0200

    Transfert des vidéos des Addons dans le component Video

--- src/components/addon/videos.tsx
+++ src/components/addon/videos.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { getById, selectOne, oneByTag } from "../../functions/selectors";
+import { resolve_video_click } from "../../functions/video";
 import Carousel from "../Carousel";
+import Video from "../Video";
 import { useImages } from '../contexts/images-provider';
 import { InmodePanel_Addon_Interface } from "../interfaces";
 import NoPicture from "../NoPic/no-picture";
@@ -20,58 +21,10 @@ const AddonVideos = ({ videos = [], title = "", name = "", sensible = false}:Add
         percentPosition: false,
         draggable: true,
     });
-    
-
-    const resolve_click = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      e.preventDefault();
-      if(e.currentTarget.id === "video-iframe" || e.currentTarget.classList.contains('close-pic')) {
-        let _temp:any = oneByTag('main');
-        _temp && _temp.style.removeProperty('z-index');
-        _temp = getById('video-iframe');
-        _temp && _temp.classList.remove('opened');
-        e.currentTarget.innerHTML = "";
-        _temp = selectOne('header');
-        _temp && _temp.classList.remove('video-opened');
-        _temp = selectOne('.header-mini-menu');
-        _temp && _temp.classList.remove('video-opened');
-      }
-    }
-
-    const openVideo = (e:React.MouseEvent<HTMLDivElement, MouseEvent>, url:string) => {
-      e.preventDefault();
-      if(url.includes('youtube')) {
-        url = url.replace('watch?v=', 'embed/') + '?autoplay=1&amp;autohide=1&amp;fs=1&amp;rel=0&amp;hd=1&amp;wmode=transparent&amp;enablejsapi=1&amp;html5=1';
-      }
-      else if(url.includes('vimeo')) {
-        url = url.replace('https://vimeo.com/', '//player.vimeo.com/video/') + '?autoplay=1&hd=1&show_title=1&show_byline=1&show_portrait=0&fullscreen=1';
-      }
-      let _temp:any = selectOne('header');
-      _temp && _temp.classList.add('video-opened');
-      _temp = selectOne('.header-mini-menu');
-      _temp && _temp.classList.add('video-opened');
-      _temp = oneByTag('main');
-      _temp && _temp.style.setProperty('z-index', 4);
-      _temp = getById('video-iframe');
-      _temp && _temp.classList.add('opened');
-      let iframe = '';
-      iframe += '<img class="close-pic" src=' + images.getOne('closeWhiteIcon').publicURL + ' onclick="resolve_click(e)"/>';
-      iframe += '<iframe ';
-      iframe += 'allowfullscreen="allowfullscreen" ';
-      iframe += 'allow="autoplay; fullscreen" ';
-      iframe += 'src="' + url +'" ';
-      iframe += 'scrolling="no"';
-      iframe += '></iframe>';
-      _temp = getById('video-iframe');
-      if(_temp) {_temp.innerHTML = iframe;}
-    }
-
-    const resolveVideoClick = (e:React.MouseEvent<HTMLDivElement, MouseEvent>, url:string) => {
-      openVideo(e, url);
-    }
 
     return (
         <div className="videos-slide">
-          <div id="video-iframe" onClick={(e) => {resolve_click(e);}}></div>
+          <div id="video-iframe" onClick={(e) => {resolve_video_click(e);}}></div>
           <div className="title">
             {title}
           </div>
@@ -83,24 +36,7 @@ const AddonVideos = ({ videos = [], title = "", name = "", sensible = false}:Add
                 <NoPicture from ="addon-videos"/>
               :
               videos.length < 3 ?
-                videos.map((video, key) => {
-                  return (
-                    <div
-                      className=" few-videos poster video"
-                      onMouseDown={(e) => {resolveVideoClick(e, video.url || '');}}
-                      onMouseUp={(e) => {resolveVideoClick(e, video.url || '');}}
-                      onClick={(e) => {resolveVideoClick(e, video.url || '');}}
-                      key={key}
-                    >
-                      <img
-                        src={video.poster && video.poster.localFile.childImageSharp.fluid.srcWebp}
-                        alt={`addon-videos-${key}`}
-                      />
-                      <span className="video-bg"></span>
-                      <span className="video-play-btn"></span>
-                    </div>
-                  );
-                })
+                videos.map((video, key) => <Video few={true} video={{...video, alt: `addon-videos-${key}`}} key={key}/>)
                 :
                 <Carousel
                     id={`carousel-videos-${name}`}
@@ -108,24 +44,7 @@ const AddonVideos = ({ videos = [], title = "", name = "", sensible = false}:Add
                     classList={'carousel-videos transition'}
                 >
                   {/* {[...(videos), ...(videos)].map((video, index) => { */}
-                  {videos.map((video, key) => {
-                      return (
-                        <div
-                          className="poster video"
-                          onMouseDown={(e) => {resolveVideoClick(e, video.url || '');}}
-                          onMouseUp={(e) => {resolveVideoClick(e, video.url || '');}}
-                          onClick={(e) => {resolveVideoClick(e, video.url || '');}}
-                          key={key}
-                        >
-                          <img
-                            src={video.poster && video.poster.localFile.childImageSharp.fluid.srcWebp}
-                            alt={`addon-videos-${key}`}
-                          />
-                          <span className="video-bg"></span>
-                          <span className="video-play-btn"></span>
-                        </div>
-                      );
-                    })
+                  {videos.map((video, key) => <Video few={false} video={{...video, alt: `addon-videos-${key}`}} key={key}/>)
                   }
                 </Carousel>
             }
