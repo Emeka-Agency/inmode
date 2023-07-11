591b5b843b0ca54e8c90f90d08f3a733a7c62859
kratoar <mael.fallet@gmail.com>
2023-07-11 02:37:32 +0200

    Ajout de la zone de Newsletter sur la page /thanks

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
+import Newsletter from "../components/newsletter";
 
 const ContainerStyle = {
     padding: '40px 32px',
@@ -28,13 +29,14 @@ const ThanksPage = (datas:ThanksPage) => {
 
   return (
     <Layout title="404">
-      <SEO title="404"/>
-      <section style={ContainerStyle}>
-        <div>
-            <p style={ThanksStyle}>Thank you for your interest in InMode.</p>
-            <p style={SubtextStyle}>We have received your request and will be in touch shortly.</p>
-        </div>
-    </section>
+        <SEO title="404"/>
+        <section style={ContainerStyle}>
+            <div>
+                <p style={ThanksStyle}>Thank you for your interest in InMode.</p>
+                <p style={SubtextStyle}>We have received your request and will be in touch shortly.</p>
+            </div>
+        </section>
+        <Newsletter/>
     </Layout>
   );
 }
