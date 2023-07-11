309cb427cf2638674a6d0b362816154ae8249cc8
kratoar <mael.fallet@gmail.com>
2023-07-11 02:35:25 +0200

    Page de remerciements

---
+++ src/pages/thanks.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
+
+const ContainerStyle = {
+    padding: '40px 32px',
+    textAlign: 'center',
+    backgroundColor: 'var(--pearl)',
+};
+
+const ThanksStyle = {
+    fontSize: '32px',
+    fontWeight: 600,
+    lineHeight: '40px',
+    marginBottom: '16px',
+    color: 'var(--teal)',
+};
+
+const SubtextStyle = {
+    fontSize: '16px',
+    lineHeight: '24px',
+    marginBottom: '16px',
+    color: 'var(--midnight)',
+};
+
+const ThanksPage = (datas:ThanksPage) => {
+
+  return (
+    <Layout title="404">
+      <SEO title="404"/>
+      <section style={ContainerStyle}>
+        <div>
+            <p style={ThanksStyle}>Thank you for your interest in InMode.</p>
+            <p style={SubtextStyle}>We have received your request and will be in touch shortly.</p>
+        </div>
+    </section>
+    </Layout>
+  );
+}
+
+interface ThanksPage {
+
+};
+
+export default ThanksPage;
