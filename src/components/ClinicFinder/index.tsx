a157a443ebc2b09192dbc1a471bb96a2bcd2373d
kratoar <mael.fallet@gmail.com>
2023-09-11 23:22:19 +0200

    Récupération des cliniques depuis Sheets

--- src/components/ClinicFinder/index.tsx
+++ src/components/ClinicFinder/index.tsx
@@ -25,38 +25,16 @@ const ClinicalFinder = ({}:ClinicalFinder_Interface) => {
     // const [rest, setRest]:[number, React.Dispatch<number>] = React.useState(0);
 
     const addClinics = function(offset:string|null = null, records:Airtable_Clinic_Interface[]|[] = []) {
-
-        const fields = ["name", "address", "shire", "city", "zip_code", "doctor", "mail", "url", "number", "treatments", "region"];
-        const sortCriteres = ['name'];
-        const sortDirections = ['desc'];
-        const sortBy = Array(sortCriteres).map((el, index) => 
-            `sort%5B0%5D%5Bfield%5D=${sortCriteres[index]}&sort%5B0%5D%5Bdirection%5D=${sortDirections[index] ?? 'desc'}`
-        ).join('&');
-
-        // _log(fields);
-        // _log(sortBy);
-        // _log(`${process.env.AIRTABLE_CLINICS}?${sortBy}&${fields.map(el => "fields%5B%5D="+el).join("&")}&maxRecords${offset == null ? '' : `&offset=${offset}`}`);
-        
-        fetch(
-            `${process.env.AIRTABLE_CLINICS}?${sortBy}&${fields.map(el => "fields%5B%5D="+el).join("&")}&maxRecords${offset == null ? '' : `&offset=${offset}`}`,
-            {headers: new Headers({"Authorization" : `Bearer ${process.env.AIRTABLE_KEY}`})}
-        )
-        .then(res => res.json())
-        .then((res:{offset:string|null, records:{fields: Airtable_Clinic_Interface}[]}) => {
-            let reg = regions;
-            res.records && res.records.map((rec: {fields:Airtable_Clinic_Interface}) => {
-                if(rec.fields && rec.fields.region && reg.indexOf(rec.fields.region || "") == -1) {
-                    reg = [...reg, rec.fields.region];
-                }
-            });
-            setRegions(reg);
-            if(res.offset) {
-                addClinics(res.offset, records.concat(res.records.map(rec => rec.fields && rec.id ? {id: rec.id, ...rec.fields} : rec)));
+        fetch(`${process.env.INMODE_BACK}/api/get-datas?type=clinics`)
+        .then(p => handlePromise(p, "json"))
+        .then((res:{status:number, datas: Airtable_Clinic_Interface[], rows: number}) => {
+            if(Array.isArray(res.datas) && res.datas.length > 0) {
+                setClinics(res.datas);
             }
             else {
-                setClinics(records.concat(res.records.map(rec => rec.fields && rec.id ? {id: rec.id, ...rec.fields} : rec)));
-                setLoading(false);
+                setClinics([]);
             }
+            setLoading(false);
         })
         .catch(err => _error(err));
     }
