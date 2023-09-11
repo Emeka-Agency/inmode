b14c9dc3a1dd3ed62a0dfcc1031f3fd0e77b7e6c
kratoar <mael.fallet@gmail.com>
2023-09-11 23:23:07 +0200

    Récupération des événements depuis Sheets

--- src/pages/events/workshops.tsx
+++ src/pages/events/workshops.tsx
@@ -7,51 +7,24 @@ import SEO from "../../components/seo";
 
 import "../../components/events/events.css";
 import { _group, _groupEnd, _log } from "../../functions/logger";
+import { handlePromise } from "../../functions/tools";
 
 const WorkshopsPage = (datas:WorkshopsPage) =>  {
 
     const [events, setEvents]:[Airtable_Event_Interface[]|[], React.Dispatch<Airtable_Event_Interface[]|[]>] = React.useState(Array());
     const [loading, setLoading]:[boolean, React.Dispatch<boolean>] = React.useState(true);
 
-    const loadEvents = async function(offset:string|null = null, records:Airtable_Event_Interface[]|[] = [], __type:string|null = null) {
-        const fields = ["EventName", "Start", "End", "Practitioner", "Address", "Place", "PlaceURL", "Addons", "EventType", "EventDescription", "MapsLink", "VideoURL", "Picture"];
-        const sortCriteres = ['Start'];
-        const sortDirections = ['desc'];
-        const sortBy = Array(sortCriteres).map((el, index) => 
-            `sort%5B0%5D%5Bfield%5D=${sortCriteres[index]}&sort%5B0%5D%5Bdirection%5D=${sortDirections[index] ?? 'desc'}`
-        ).join('&');
-        var filterBy = '';
-        if(typeof __type == "string") {
-            filterBy = `&filterByFormula={EventType}='${__type}'`;
-        }
-    
-        _group(fields);
-        _log(sortBy);
-        _log(filterBy);
-        _log(`${process.env.AIRTABLE_EVENTS}?${sortBy}&${fields.map(el => "fields%5B%5D="+el).join("&")}&maxRecords${offset == null ? '' : `&offset=${offset}`}${filterBy}`);
-        _groupEnd();
-        
-        await fetch(
-            `${process.env.AIRTABLE_EVENTS}?${sortBy}&${fields.map(el => "fields%5B%5D="+el).join("&")}&maxRecords${offset == null ? '' : `&offset=${offset}`}${filterBy}`,
-            {headers: new Headers({"Authorization" : `Bearer ${process.env.AIRTABLE_KEY}`})}
-        )
-        .then(res => res.json())
-        .then((res:{offset:string|null, records:{fields: Airtable_Event_Interface}[]}) => {
-            _log(res.offset != undefined);
-            _log(res.records.length == 0);
-            if(res.records.length == 0) {
+    const loadEvents = async function(__type:string|null = null) {
+        await fetch(`${process.env.INMODE_BACK}/api/get-datas?type=events`)
+        .then(p => handlePromise(p, "json"))
+        .then((res:{status:string, datas:Airtable_Event_Interface[]}) => {
+            if(res.datas.length == 0) {
                 setLoading(false);
                 return false;
             }
-            if(res.offset != undefined) {
-                // let news = res.records.map(rec => rec.fields && rec.id ? {id: rec.id, ...rec.fields} : rec);
-                // loadEvents(res.offset, [...records]);
-                loadEvents(res.offset, records.concat(res.records.map(rec => rec.fields && rec.id ? {id: rec.id, ...rec.fields} : rec)));
-                return true;
-            }
             else {
                 setLoading(false);
-                setEvents(records.concat(res.records.map(rec => rec.fields && rec.id ? {id: rec.id, ...rec.fields} : rec)));
+                setEvents(res.datas.filter(event => event.EventType == __type));
                 return true;
             }
         })
@@ -59,7 +32,7 @@ const WorkshopsPage = (datas:WorkshopsPage) =>  {
     }
 
     React.useEffect(() => {
-        loading && loadEvents(null, [], "Workshop");
+        loading && loadEvents("Workshop");
     }, [events]);
 
     return (
