4f3018f7250d50a105ca85cf627d2cf274b1c3a5
kratoar <mael.fallet@gmail.com>
2023-11-25 23:29:47 +0100

    Possibilité de passer le type d'événement pour NoEvents

--- src/components/events/no-events.tsx
+++ src/components/events/no-events.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-const NoEvents = ({}:NoEvents) => {
+const NoEvents = ({type}:NoEvents) => {
     return (
         <div className="no-events">
-            Il n'y a actuellement aucun évènement à venir
+            Il n'y a actuellement aucun {type ?? "évènement"} à venir
         </div>
     );
 };
 
 interface NoEvents {
-
+    type: string;
 };
 
 export default NoEvents;
