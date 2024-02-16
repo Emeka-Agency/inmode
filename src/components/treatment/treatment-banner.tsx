8b8431544862c373774ebe9483851ac763ea83c3
kratoar <mael.fallet@gmail.com>
2024-02-16 02:48:45 +0100

    Utilisation du nom du traitement dans la bannière des pages de traitement

--- src/components/treatment/treatment-banner.tsx
+++ src/components/treatment/treatment-banner.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import { resolveImg, resolveImgSet } from "../../functions/tools";
 import { InmodePanel_Treat_Banner_Interface } from "../interfaces";
 
-const TreatmentBanner = ({ datas, variant = "teal" }:TreatmentBanner) => {
+const TreatmentBanner = ({ datas, variant = "teal", name }:TreatmentBanner) => {
 
     // TODO récupérer images et vidéos pour chaque produit
     return (
-        <div className="treatment-banner" data-variant={variant}>
+        <div className="treatment-banner" data-treatment={name} data-variant={variant}>
             <div className="top-transition"></div>
-            <div className="treatment-banner-media">
+            <div
+                className="treatment-banner-media"
+                // style={{
+                //     backgroundImage: `url(${resolveImg(datas?.picture)})`,
+                //     backgroundSize: "cover",
+                //     backgroundPosition: "center",
+                //     backgroundRepeat: "no-repeat",
+                // }}
+            >
                 <img
                     className="user-select-none"
                     src={resolveImg(datas?.picture)}
@@ -24,6 +32,7 @@ const TreatmentBanner = ({ datas, variant = "teal" }:TreatmentBanner) => {
 interface TreatmentBanner {
     datas?: InmodePanel_Treat_Banner_Interface;
     variant?: string;
+    name?: string;
 };
 
 export default TreatmentBanner;
