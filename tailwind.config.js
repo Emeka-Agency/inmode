531426fc666ecad15c194cbf861016f4ac87d892
kratoar <mael.fallet@gmail.com>
2024-11-16 00:44:07 +0100

    Spécification des chemins de travail acceptés pour TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path based on where your components/pages are
+        "./src/pages/**/*.{js,jsx,ts,tsx}", // Adjust this path based on where your components/pages are
+        "./src/components/**/*.{js,jsx,ts,tsx}", // Adjust this path based on where your components/pages are
     ],
     theme: {
         extend: {},
